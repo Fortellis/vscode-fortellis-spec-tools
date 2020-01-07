30eced7b671aa24d335be806501b7ba496d44c92
Jared Jones <Jared.Jones@cdk.com>
2020-01-07 15:53:28 -0600

    Adding diagnostics to highlight errors based on validation

--- extension.js
+++ extension.js
@@ -1,30 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const validate = require("@fortellis/spec-validator");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const diagnosticCollection = vscode.languages.createDiagnosticCollection();
+
 function activate(context) {
+  const validateAction = vscode.commands.registerTextEditorCommand(
+    "extension.validateSpec",
+    validateSpec
+  );
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "fortellis-spec-validator" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(validateAction);
+  context.subscriptions.push(diagnosticCollection);
 }
-exports.activate = activate;
+
+function validateSpec(editor) {
+  validate(editor.document.getText())
+    .then(res => {
+      const diagnostics = res.map(err => {
+        return new vscode.Diagnostic(err.range, err.message);
+      });
+      diagnosticCollection.set(editor.document.uri, diagnostics);
+    })
+    .catch(err => {
+      vscode.window.showInformationMessage(err);
+    });
+}
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
