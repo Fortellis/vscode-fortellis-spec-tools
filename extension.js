c078c6cd6789493fa5d8135be3b8d5ed5578596c
Trentin Priebe <trentin.priebe@cdk.com>
2020-01-07 16:59:24 -0500

    Merging my changes in

--- extension.js
+++ extension.js
@@ -3,14 +3,30 @@ const validate = require("@fortellis/spec-validator");
 
 const diagnosticCollection = vscode.languages.createDiagnosticCollection();
 
+let timeout = undefined;
+
 function activate(context) {
   const validateAction = vscode.commands.registerTextEditorCommand(
     "extension.validateSpec",
     validateSpec
   );
 
+  vscode.workspace.onDidChangeTextDocument(event => {
+    if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+        triggerValidateSpec();
+    }
+  }, null, context.subscriptions);
+
   context.subscriptions.push(validateAction);
   context.subscriptions.push(diagnosticCollection);
+}
+
+function triggerValidateSpec() {
+    if (timeout) {
+        clearTimeout(timeout);
+        timeout = undefined;
+    }
+    timeout = setTimeout(validateSpec, 2000);
 }
 
 function validateSpec(editor) {
@@ -20,6 +36,7 @@ function validateSpec(editor) {
         return new vscode.Diagnostic(err.range, err.message);
       });
       diagnosticCollection.set(editor.document.uri, diagnostics);
+      vscode.window.showErrorMessage('Validation failed!', ...res.map((err => err.message)));
     })
     .catch(err => {
       vscode.window.showInformationMessage(err);
